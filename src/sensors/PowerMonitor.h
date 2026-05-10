0f09270973988f4b3bbbf915e2cbb6d7c381c6a8
willianrupert <willianrupert@gmail.com>
2026-05-10 12:43:47 -0300

    feat(firmware): update PowerMonitor header for INA219

--- src/sensors/PowerMonitor.h
+++ src/sensors/PowerMonitor.h
@@ -1,16 +1,18 @@
 // src/sensors/PowerMonitor.h
 #pragma once
 #include <stdint.h>
+#include <Adafruit_INA219.h>
 
 class PowerMonitor {
 public:
   static PowerMonitor& P1() { static PowerMonitor i; return i; }
-  static PowerMonitor& P2() { static PowerMonitor i; return i; } // Para suporte a interlock e P2 futuramente
+  static PowerMonitor& P2() { static PowerMonitor i; return i; }
   
   bool init(uint8_t i2c_addr);
   bool readWithRecovery(float& out_ma);
 
 private:
   uint8_t _addr = 0x40;
+  Adafruit_INA219 _ina;
   PowerMonitor() = default;
 };
