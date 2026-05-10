f44c44ee101ff56171516c5c691e98bb17958338
willianrupert <willianrupert@gmail.com>
2026-05-10 12:43:47 -0300

    feat(firmware): implement systemScore logic in HealthMonitor

--- src/health/HealthMonitor.cpp
+++ src/health/HealthMonitor.cpp
@@ -14,3 +14,14 @@ void HealthMonitor::report(SensorID id, bool ok, const char* context_reason) {
 bool HealthMonitor::usable(SensorID id) const {
   return _failures[(int)id] < ConfigManager::instance().cfg.sensor_fail_threshold;
 }
+
+int HealthMonitor::systemScore() const {
+  int score = 0;
+  if (usable(SensorID::GM861))     score += SCORE_GM861;
+  if (usable(SensorID::KEYPAD))    score += SCORE_KEYPAD;
+  if (usable(SensorID::HX711))     score += SCORE_HX711;
+  if (usable(SensorID::MMWAVE))    score += SCORE_MMWAVE;
+  if (usable(SensorID::INA219_P1)) score += SCORE_INA219_P1;
+  if (usable(SensorID::INA219_P2)) score += SCORE_INA219_P2;
+  return score;
+}
